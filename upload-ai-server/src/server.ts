c0832db33cd5cfd301035ceca66d6d05203f9431
Vinícius Lima <villylss@gmail.com>
2023-09-13 23:29:10 -0300

    feature: finalizadas as rotas get-all-prompts e upload-video, rota create-transcription iniciada

--- upload-ai-server/src/server.ts
+++ upload-ai-server/src/server.ts
@@ -1,10 +1,13 @@
 import { fastify } from 'fastify'
+import { getAllPromptsRoute } from './routes/get-all-prompts'
+import { uploadVideoRoute } from './routes/upload-video'
+import { createTranscriptionRoute } from './routes/create-transcription'
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello, world!'
-})
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
 
 app.listen({
   port: 3333,
