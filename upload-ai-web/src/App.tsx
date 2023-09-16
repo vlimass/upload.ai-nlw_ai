492d55d42d9085f4ef72807ba6665c4c670e9122
Vinícius Lima <villylss@gmail.com>
2023-09-15 21:15:35 -0300

    feature: videoId, template e temperature disponíveis no app.tsx para a integração final

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Github, Wand2 } from 'lucide-react';
 import { Button } from "./components/ui/button";
 import { Separator } from './components/ui/separator';
@@ -6,8 +7,18 @@ import { Label } from './components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/ui/select';
 import { Slider } from './components/ui/slider';
 import { VideoInputForm } from './components/video-input-form';
+import { PromptSelect } from './components/prompt-select';
 
 export function App() {
+  const [temperature, setTemperature] = useState(0.5)
+  const [videoId, setVideoId] = useState<string | null>(null)
+
+  function handlePromptSelected(template: string) {
+    console.log(template)
+  }
+
+
+
   return (
     <div className='min-h-screen flex flex-col'>
       <div className="px-6 py-3 flex items-center justify-between border-b">
@@ -47,22 +58,15 @@ export function App() {
         </div>
 
         <aside className='w-80 space-y-6'>
-          <VideoInputForm />
+          <VideoInputForm onVideoUploaded={setVideoId} />
 
           <Separator />
 
           <form className='space-y-6'>
             <div className='space-y-2'>
               <Label>Prompt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt..."/>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value='title'>Título do Youtube</SelectItem>
-                  <SelectItem value='description'>Descrição do Youtube</SelectItem>
-                </SelectContent>
-              </Select>
+
+              <PromptSelect onPromptSelected={handlePromptSelected} />
             </div>
 
             <div className='space-y-2'>
@@ -89,6 +93,8 @@ export function App() {
                 min={0}
                 max={1}
                 step={0.05}
+                value={[temperature]}
+                onValueChange={value => setTemperature(value[0])}
               />
 
               <span className='block text-xs text-muted-foreground italic leading-relaxed'>
